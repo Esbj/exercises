dbbe2117ea56765521ae71bd8d098acddbbf3273
Alexander <alex.esbj@gmail.com>
2019-09-19 14:54:42 +0200

    Löste del 1
    Och halva 2

---
+++ Exercise_IF-Satsen1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise_IF_Satsen1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Är det fint väder?");
+            string fintVäder = Console.ReadLine();
+
+            if (fintVäder == "J"){
+                Console.WriteLine("Vi går på en promenad!");
+            }else if (fintVäder == "N"){
+                Console.WriteLine("Vi stannar inne och läser en bok!");
+            }
+        }
+    }
+}
